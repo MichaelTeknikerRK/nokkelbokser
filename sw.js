64011c702f4cfe2c02d33a4da764155c3b38953e
MichaelTeknikerRK <michael.aglen@rana.kommune.no>
2026-04-13 12:44:39 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='nb-v3';
-const STATIC=['/nokkelbokser/','/nokkelbokser/index.html','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  if(/supabase|geonorge|openstreetmap/.test(e.request.url))return;
-  e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request)));
-});
